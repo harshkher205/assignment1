453607af603fe845ad446fc3e0584358f43e3488
harshkher205 <hkher98@gmail.com>
2026-02-05 17:34:18 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter a number: "))
+num2 = int(input("enter a number: "))
+print("                   ")
+print("addition:",num1+num2)
+print("subtraction:",num1-num2)
+print("multiplication:",num1*num2)
+print("divide:",num1/num2)
+
+
+
